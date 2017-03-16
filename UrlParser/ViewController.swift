474e01b1ce899bd3d91fbe270cc4c8deb3716b68
Christopher Eliasson <Christopher.Eliasson@hiq.se>
2017-03-16 16:04:09 +0100

    Added project

---
+++ UrlParser/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  UrlParser
+//
+//  Created by Christopher Eliasson on 2017-03-16.
+//  Copyright © 2017 Christopher Eliasson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
